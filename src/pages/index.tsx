3258df2cb45fa15822af2565cb12c0cbed089de6
Jimmi Nielsen <jimmi.nielsen@akqa.com>
2021-04-16 10:25:04 +0200

    add graphql-codegen to auto generate types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,42 @@ import styles from "@styles/Home.module.css";
 import { Button } from "@components/Button/Button";
 import { getAllPostsForHome } from "../pages/api/graphcms";
 import Link from "next/link";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 export default function Home({ posts }) {
+    const client = new ApolloClient({
+        uri:
+            "https://api-eu-central-1.graphcms.com/v2/ckn7guqyzd7vg01xrgaqo8ee0/master",
+        cache: new InMemoryCache(),
+    });
+
     const heroPost = posts[0];
 
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <ApolloProvider client={client}>
+            <div className={styles.container}>
+                <Head>
+                    <title>Create Next App</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
 
-            <main className={styles.main}>
-                <Button children="Click me!" />
+                <main className={styles.main}>
+                    <Button children="Click me!" />
 
-                <div>title: {heroPost.title}</div>
-                <div>date: {heroPost.date}</div>
-                <div>author: {heroPost.author.name}</div>
-                <div>slug: {heroPost.slug}</div>
-                <div>excerpt: {heroPost.excerpt}</div>
+                    <div>title: {heroPost.title}</div>
+                    <div>date: {heroPost.date}</div>
+                    <div>author: {heroPost.author.name}</div>
+                    <div>slug: {heroPost.slug}</div>
+                    <div>excerpt: {heroPost.excerpt}</div>
 
-                <Link as={`/posts/${heroPost.slug}`} href="/posts/[slug]">
-                    <a style={{ textDecoration: "underline" }}>
-                        {heroPost.slug}
-                    </a>
-                </Link>
-            </main>
-        </div>
+                    <Link as={`/posts/${heroPost.slug}`} href="/posts/[slug]">
+                        <a style={{ textDecoration: "underline" }}>
+                            {heroPost.slug}
+                        </a>
+                    </Link>
+                </main>
+            </div>
+        </ApolloProvider>
     );
 }
 
